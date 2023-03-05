3a9772c1863087e50b65447eec97c8bf0e714498
lumpn <lumpn@users.noreply.github.com>
2023-03-05 15:36:35 +0800

    Validating basic proofs

---
+++ Lumpn.ZeldaProof.Test/GraphTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Lumpn.ZeldaProof.Test
+{
+    [TestFixture]
+    public sealed class GraphTest
+    {
+        [Test]
+        public void CreateGraph()
+        {
+            var builder = new GraphBuilder();
+            builder.addItem(0, "A");
+            builder.addTransition(0, 1, "A");
+
+            var graph = builder.build();
+            var verifier = new GraphValidator();
+            
+            var result = verifier.Verify(graph);
+            Assert.IsTrue(result);
+        }
+    }
+}
